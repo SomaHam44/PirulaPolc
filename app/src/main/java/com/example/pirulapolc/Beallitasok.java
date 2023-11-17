018db9dfd1396ee2b4fd0eed826bcdde186bd810
feketeand <afekete.andrea@gmail.com>
2023-11-17 14:11:05 +0100

    beállítások oldal kód

--- app/src/main/java/com/example/pirulapolc/Beallitasok.java
+++ app/src/main/java/com/example/pirulapolc/Beallitasok.java
@@ -4,19 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class Beallitasok extends AppCompatActivity {
+
+    private Button btnVissza;
+
+    private Button btnTovabb;
+
+    private EditText editTextNev;
+
+    private EditText editTextSzulDatum;
+
+    private EditText editTextTajSzam;
+
+    private CheckBox checkboxRendszeresSzedes;
+
+    private androidx.recyclerview.widget.RecyclerView idopontLista;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beallitasok);
+
+        btnVissza.setOnClickListener(view -> {
+            Intent fooldalra = new Intent(Beallitasok.this, MainActivity.class);
+            startActivity(fooldalra);
+            finish();
+        });
+
+        btnVissza.setOnClickListener(view -> {
+            Intent menure = new Intent(Beallitasok.this, Menu.class);
+            startActivity(menure);
+            finish();
+        });
     }
-
-    btnIndito.setOnClickListener(view -> {
-        Intent beallitasokhoz = new Intent(MainActivity.this, beallitasok.class);
-        startActivity(beallitasokhoz);
-        finish();
-    });
-
 }
