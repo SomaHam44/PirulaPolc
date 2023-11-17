2ed9759e5fd914cd8d13ac64b883a7edc044faf0
SomaHam44 <somasolti@gmail.com>
2023-11-17 12:18:21 +0100

    Kezdőoldal kód, beállításokra ugrás

--- app/src/main/java/com/example/pirulapolc/MainActivity.java
+++ app/src/main/java/com/example/pirulapolc/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.pirulapolc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnIndito;
+    private TextView alkalmazasNeve;
+    private TextView bevezetoSzoveg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        btnIndito.setOnClickListener(view -> {
+            Intent beallitasokhoz = new Intent(MainActivity.this, beallitasok.class);
+            startActivity(beallitasokhoz);
+            finish();
+        });
+    }
+
+
+
+    private void init() {
+        btnIndito = findViewById(R.id.btnIndito);
+        alkalmazasNeve = findViewById(R.id.alkalmazasNeve);
+        bevezetoSzoveg = findViewById(R.id.bevezetoSzoveg);
     }
 }
