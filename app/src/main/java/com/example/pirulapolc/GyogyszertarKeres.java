2b258141effefc6e14bec3abcaa3e10abebf057e
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-11-21 20:37:12 +0100

    Gyógyszertár kereső oldal

--- app/src/main/java/com/example/pirulapolc/GyogyszertarKeres.java
+++ app/src/main/java/com/example/pirulapolc/GyogyszertarKeres.java
@@ -2,13 +2,31 @@ package com.example.pirulapolc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.maps.MapView;
 
 public class GyogyszertarKeres extends AppCompatActivity {
+    private MapView gyogyszertarTerkep;
+    private Button btnVissza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gyogyszertar_keres);
+        init();
+
+        btnVissza.setOnClickListener(view -> {
+            Intent visszaMenube = new Intent(GyogyszertarKeres.this, Menu.class);
+            startActivity(visszaMenube);
+            finish();
+
+        });
+    }
+    private void init() {
+        gyogyszertarTerkep = findViewById(R.id.gyogyszertarTerkep);
+        btnVissza = findViewById(R.id.btnVissza);
     }
 }
